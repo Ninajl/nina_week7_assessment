36a02c15b1138e826a5b185ad75e5e702d87a4c0
Nina Lisitsa <Ninajl1@Ninas-MacBook-Pro.local>
2015-02-23 15:39:00 -0700

    Add courses controller and view. Able to create new course

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,42 @@
+class CoursesController <ApplicationController
+
+  def index
+    @courses = Course.all
+  end
+
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.new(course_params)
+    if @course.save
+      redirect_to courses_path
+    else
+      rendor :new
+    end
+  end
+
+  def show
+    @course = Course.find(params[:id])
+  end
+
+  def edit
+    @course = Course.find(params[:id])
+  end
+
+  def update
+  end
+
+  def destroy
+    @course = Course.find(params[:id])
+    if @course.destroy
+      redirect_to courses_path
+    end
+  end
+
+  private
+  def course_params
+    params.require(:course).permit(:title, :day, :night)
+  end
+end
